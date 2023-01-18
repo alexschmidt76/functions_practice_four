8cd5474b9d904f945f8b5b96e690d359e6f1baff
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-17 22:46:47 -0600

    pascal function complete

--- assignment.py
+++ assignment.py
@@ -37,4 +37,27 @@ print('Test num_within():')
 print(num_within(5, 3, 10))
 print(num_within(1, 3, 10))
 
+# 5
+def pascal(n):
+    # error case
+    if n < 1:
+        print(f'{n} is an invalid number of rows')
+    # set current row to 1 and calculate each row until n
+    current_row = 1
+    while current_row <= n:
+        # row always starts with 1
+        row = [1]
+        # find each element of current row
+        for i in range(1, current_row):
+            # formula finds element of pascals triangle 
+            # given the row and position in row
+            element = (row[i - 1] * (current_row - i)) / i
+            row.append(int(element))
+        print(row)
+        current_row += 1
 
+print('Test pascal():')
+pascal(4)
+pascal(1)
+pascal(-9)
+pascal(10)
