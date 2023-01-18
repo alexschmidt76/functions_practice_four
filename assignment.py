c56a024b99228153249f8bcbf41c5749f7438dc3
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-17 23:05:07 -0600

    added alternate version of pascal function using recursion instead of formula

--- assignment.py
+++ assignment.py
@@ -5,6 +5,7 @@ def max_num(n1, n2, n3):
 print('Test max_num():')
 print(max_num(1, 2, 3))
 print(max_num(3, 55, 2))
+print('\n')
 
 # 2
 def mult_list(list):
@@ -17,6 +18,7 @@ def mult_list(list):
 
 print('Test mult_list():')
 print(mult_list([4, 6, 9]))
+print('\n')
 
 # 3
 def rev_string(str):
@@ -28,6 +30,7 @@ def rev_string(str):
 
 print('Test rev_string():')
 print(rev_string('alex'))
+print('\n')
 
 # 4
 def num_within(n, beg, end):
@@ -36,17 +39,53 @@ def num_within(n, beg, end):
 print('Test num_within():')
 print(num_within(5, 3, 10))
 print(num_within(1, 3, 10))
+print('\n')
 
 # 5
+# using recursion
+def rec_pascal(n):
+    # error case
+    if n < 1:
+        return f'{n} is an invalid number of rows'
+
+    # row always starts with 1
+    row = [1]
+
+    # base case
+    if n == 1:
+        print(row)
+        return row
+    
+    # recursion case
+    # find previous row
+    previous = rec_pascal(n - 1)
+    # use previous row to find elements of current row
+    for i in range(1, len(previous)):
+        current = previous[i - 1] + previous[i]
+        row.append(current)
+    row.append(1)
+    print(row)
+    return(row)
+
+print('Test rec_pascal():')
+rec_pascal(4)
+rec_pascal(1)
+rec_pascal(-9)
+rec_pascal(10)
+print('\n')
+
+# using formula
 def pascal(n):
     # error case
     if n < 1:
         print(f'{n} is an invalid number of rows')
+
     # set current row to 1 and calculate each row until n
     current_row = 1
     while current_row <= n:
         # row always starts with 1
         row = [1]
+        
         # find each element of current row
         for i in range(1, current_row):
             # formula finds element of pascals triangle 
@@ -60,4 +99,5 @@ print('Test pascal():')
 pascal(4)
 pascal(1)
 pascal(-9)
-pascal(10)
+pascal(10) 
+print('\n')
